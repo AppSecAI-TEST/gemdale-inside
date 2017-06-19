58f625ba57eb668172039c70c5513a7318f5b94d
xiaqi <274572294@qq.com>
2017-06-19 22:15:47 +0800

    修改bussiness usercontroller

--- inside-business/inside-business-web/src/main/java/com/inside/business/controller/UserController.java
+++ inside-business/inside-business-web/src/main/java/com/inside/business/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     private UserService userService;
 
     /**
-     * 2017/06/19
+     * 2017/06/191111111111111111
      *  根据ID查询
      * @param userId
      * @return
